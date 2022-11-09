dcb264955d78a0d0835d052ce4756da75a11be6a
Dmitriy <diste65@gmail.com>
2022-11-09 21:11:48 +0200

    gmail don't work

--- js/script.js
+++ js/script.js
@@ -101,6 +101,29 @@ $(document).ready(function(){
     validateForms('#consultation-form');
     validateForms('#consultation form');
     validateForms('#order form');
+
+    $('input[name=phone]').mask("+3 80(99) 999-99-99");
+
+
+    $('form').submit(function(e) {
+      e.preventDefault();
+      
+      if (!$(this).valid()) {
+        return;
+      }
+
+      $.ajax({
+          type: "POST",
+          url: "mailer/smart.php",
+          data: $(this).serialize()
+      }).done(function() {
+          $(this).find("input").val("");
+          $('#consultation, #order').fadeOut();
+          $('.overlay, #thanks').fadeIn('slow');
+          $('form').trigger('reset');
+      });
+      return false;
+    });
      
 
 });
