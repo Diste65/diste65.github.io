9b99d44301208ed697d169828d658121f73a5e36
Dmitriy <diste65@gmail.com>
2022-11-09 20:10:31 +0200

    validate forms

--- js/script.js
+++ js/script.js
@@ -52,25 +52,58 @@ $(document).ready(function(){
         });
       });
     
-   
+  
+      const slider = tns({
+        container: '.carousel__inner',
+        items: 1,
+        slideBy: 'page',
+        autoplay: true,
+        controls: false,
+        nav: false,
+        autoplayButtonOutput: false
+        
+      });
+      document.querySelector('.prev').addEventListener('click', function () {
+        slider.goTo('prev');
+      });
+      document.querySelector('.next').addEventListener('click', function () {
+        slider.goTo('next');
+      });
+
+  
+      function validateForms(form){
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                  },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                  required: "Пожалуйста, введите свою почту",
+                  email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
      
 
 });
-const slider = tns({
-    container: '.carousel__inner',
-    items: 1,
-    slideBy: 'page',
-    autoplay: true,
-    controls: false,
-    nav: false,
-    autoplayButtonOutput: false
-    
-  });
-  document.querySelector('.prev').addEventListener('click', function () {
-    slider.goTo('prev');
-  });
-  document.querySelector('.next').addEventListener('click', function () {
-    slider.goTo('next');
-  });
+
 
  
